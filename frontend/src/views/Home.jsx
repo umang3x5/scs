8bc9db189fa3cf21817fd3a45c4d77c81e5198ef
umang3x5 <patel3x5@uwindsor.ca>
2023-08-08 00:26:20 -0400

    added cahnge

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -16,7 +16,7 @@ import bannerImage from '../assets/images/welcome-banner.svg';
 const DEVICE_ID = new uuid.DeviceUUID().get();
 
 const Home = () => {
-  const pageName = 'HomeDC';
+  const pageName = 'HomeDC - DChanged';
   const history = useHistory();
   const { showErrorToastr } = useToastr();
 
